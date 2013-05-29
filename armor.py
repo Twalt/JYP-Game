8487e0d59614ae023c6bffa6f3903358876c79d1
Arjun Shanker <rjnshanker@gmail.com>
2013-05-29 03:14:08 -0500

    Armor inheritance works properly

--- armor.py
+++ armor.py
@@ -1,29 +1,43 @@
 from item import Item
+import random
 
 class armor(Item):
-	def getArVal(self):
-		return self.arVal
+	def __init__(self, loc):
+		super(armor, self).__init__(loc)
+		self.arVal = random.randint(self.__start__, self.__end__)
 
 class boots(armor):
-	def __init__(self):
-		self.arVal = randint(2, 8)
+	def __init__(self, loc):
+		super(boots, self).__init__(loc)
+	__start__ = 2
+	__end__ = 8
 	
 class gauntlets(armor):
-	def __init__(self):
-		self.arVal = randint(3, 9)
+	def __init__(self, loc):
+		super(gauntlets, self).__init__(loc)
+	__start__ = 3
+	__end__ = 9
 	
 class legplates(armor):
-	def __init__(self):
-		self.arVal = randint(7, 13)
+	def __init__(self, loc):
+		super(legplates, self).__init__(loc)
+	__start__ = 7
+	__end__ = 13
 	
 class shoulderplates(armor):
-	def __init__(self):
-		self.arVal = randint(9, 15)
+	def __init__(self, loc):
+		super(shoulderplates, self).__init__(loc)
+	__start__ = 9
+	__end__ = 15
 	
 class helm(armor):
-	def __init__(self):
-		self.arVal = randint(10, 16)
+	def __init__(self, loc):
+		super(helm, self).__init__(loc)
+	__start__ = 10
+	__end__ = 15
 		
 class breastplate(armor):
-	def __init__(self):
-		self.arVal = randint(12, 18)
+	def __init__(self, loc):
+		super(breastplate, self).__init__(loc)
+	__start__ = 12
+	__end__ = 18
--- main.py
+++ main.py
@@ -11,16 +11,11 @@ from item import Location
 from character import Character
 import armor
 def main():
-	y = Character(Location(45,4))
-	z = armor.helm(Location(2,2))
-	y.helm = z
-	y.helm.setLoc(Location(-2,-2))
-	f = weapon(Location(1, 1))
-	print(f.loc)
-	print(z.loc)
-	print(y.helm.loc)
-	print(f.loc)
-	return 0
+	you = Character(Location(25,35))
+	
+	xVal = randint(1, 49)
+	yVal = randint(1, 22)
+	
 
 main()
 
